66dbd8b163000e8ab105f86376c770cfc52d59fa
vishnubhadri <vishnubhadri@hotmail.com>
2021-07-16 14:37:43 +0530

    initial setup

---
+++ custom-lib/src/components/customButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-btn color="orange"> I am custom button </v-btn>
+</template>
+
+<script>
+import { VBtn } from "vuetify/lib";
+export default {
+  name: "custom-button",
+  components: { VBtn },
+};
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ custom-lib/src/components/wrapper.js
@@ -0,0 +1,1 @@
+export { default as customButton } from './customButton.vue'
